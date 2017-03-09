e7be8349040670864421447148c2da556e7c5752
kuixon <salgueiro93@gmail.com>
2017-03-09 13:03:25 +0100

    Se valida que funcionan correctamente los métodos REST del resource Usuario/s.

--- TrekkingAventura/src/test/Test.java
+++ TrekkingAventura/src/test/Test.java
@@ -7,15 +7,37 @@ public class Test {
 
 	public static void main(String[] args) {
 		// USUARIOS
+		
+		// Insertar
 		Usuario us1 = new Usuario("Usuario 1");
 		Usuario us2 = new Usuario("Usuario 2");
-		Usuario u;
-		ClientManager.getInstance().insertarUsuario(us1);
-		ClientManager.getInstance().insertarUsuario(us2);
-		u = ClientManager.getInstance().obtenerUsuarioPorId(us1.getIdUsuario());
-		System.out.println("Id: " + u.getIdUsuario());
-		u = ClientManager.getInstance().obtenerUsuarioPorId(us2.getIdUsuario());
-		System.out.println("Id: " + u.getIdUsuario());
+		Usuario us3 = new Usuario("Usuario 3");
+		
+		if (ClientManager.getInstance().insertarUsuario(us1)) {
+			System.out.println("Usuario 1 insertado correctamente.");
+		} else {
+			System.out.println("No se ha podido insertar el Usuario 1");
+		}
+		
+		if (ClientManager.getInstance().insertarUsuario(us2)) {
+			System.out.println("Usuario 2 insertado correctamente.");
+		} else {
+			System.out.println("No se ha podido insertar el Usuario 2");
+		}
+		
+		if (ClientManager.getInstance().insertarUsuario(us3)) {
+			System.out.println("Usuario 3 insertado correctamente.");
+		} else {
+			System.out.println("No se ha podido insertar el Usuario 3");
+		}
+		
+		// Obtener Usuarios
+		us1 = ClientManager.getInstance().obtenerUsuarioPorId("Usuario 1");
+		us2 = ClientManager.getInstance().obtenerUsuarioPorId("Usuario 2");
+		us3 = ClientManager.getInstance().obtenerUsuarioPorId("Usuario 3");
+		System.out.println(us1.toString());
+		System.out.println(us2.toString());
+		System.out.println(us3.toString());
 	}
 
 }
