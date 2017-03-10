6551f7379a0077f3c0c8a446d8da1c120f4fea80
kuixon <salgueiro93@gmail.com>
2017-03-10 12:50:24 +0100

    Se acaba de realizar toda la validación de los métodos del servidor REST instanciado en GAE.

--- TrekkingAventura/src/test/Test.java
+++ TrekkingAventura/src/test/Test.java
@@ -3,6 +3,7 @@ package test;
 import java.util.ArrayList;
 
 import dao.Excursion;
+import dao.Opinion;
 import dao.Usuario;
 import utilities.ClientManager;
 
@@ -10,7 +11,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// USUARIOS
-		// Insertar
+		// Insertar usuarios
 		Usuario us1 = new Usuario("Usuario 1");
 		Usuario us2 = new Usuario("Usuario 2");
 		Usuario us3 = new Usuario("Usuario 3");
@@ -52,7 +53,7 @@ public class Test {
 		System.out.println();
 		
 		// EXCURSIONES
-		// Insertar
+		// Insertar excursiones
 		Excursion exc1 = new Excursion("Ruta del Cares", "Medio", "Arenas de Cabrales", 12, "http://res.cloudinary.com/trekkingaventura/image/upload/c2a61b1cd1ac1d22_1_1.jpg", 43.253143f, -4.844181f);
 		Excursion exc2 = new Excursion("Ventana Relux", "Facil", "Karrantza Harana", 2.7, "http://res.cloudinary.com/trekkingaventura/image/upload/c2a61b1cd1ac1d22_2_2.jpg", 43.250062f, -3.411184f);
 		Excursion exc3 = new Excursion("Faro del Caballo", "Medio", "Santoña", 12, "http://res.cloudinary.com/trekkingaventura/image/upload/c2a61b1cd1ac1d22_3_3.jpg", 43.451618f, -3.425712f);
@@ -169,9 +170,122 @@ public class Test {
 		System.out.println();
 		
 		// Eliminar excursion
-		System.out.println("Eliminamos la excursión 'Ruta del Cares':");
-		ClientManager.getInstance().eliminarExcursion(excObtenida.getIdExcursion());
+		System.out.println("Eliminamos la excursión 'Ruta de los Pantaneros':");
+		ClientManager.getInstance().eliminarExcursion(7);
 		System.out.println("Excursión eliminada correctamente.");
 		System.out.println();
+		
+		// OPINIONES
+		// Insertar opiniones
+		Opinion opinion1 = new Opinion("Usuario 1", 1, "Opinion Usuario 1 sobre Ruta del Cares", "foto Usuario 1 Ruta del Cares");
+		Opinion opinion2 = new Opinion("Usuario 1", 2, "Opinion Usuario 1 sobre Faro del Caballo", "foto Usuario 1 Faro del Caballo");
+		Opinion opinion3 = new Opinion("Usuario 1", 3, "Opinion Usuario 1 sobre Ventana Relux", "foto Usuario 1 Ventana Relux");
+		Opinion opinion4 = new Opinion("Usuario 2", 1, "Opinion Usuario 2 sobre Ruta del Cares", "foto Usuario 2 Ruta del Cares");
+		Opinion opinion5 = new Opinion("Usuario 3", 1, "Opinion Usuario 3 sobre Ruta del Cares", "foto Usuario 3 Ruta del Cares");
+		Opinion opinionExistente = new Opinion();
+		opinionExistente.setIdOpinion(1);
+		opinionExistente.setIdUsuario("Usuario 1");
+		opinionExistente.setIdExcursion(1);
+		opinionExistente.setOpinion("opinion");
+		opinionExistente.setFoto("foto");
+		
+		if (ClientManager.getInstance().insertarOpinion(opinion1)) {
+			System.out.println("Opinion 1 insertada correctamente.");
+		} else {
+			System.out.println("No se ha podido insertar la Opinion 1");
+		}
+		
+		if (ClientManager.getInstance().insertarOpinion(opinion2)) {
+			System.out.println("Opinion 2 insertada correctamente.");
+		} else {
+			System.out.println("No se ha podido insertar la Opinion 2");
+		}
+		
+		if (ClientManager.getInstance().insertarOpinion(opinion3)) {
+			System.out.println("Opinion 3 insertada correctamente.");
+		} else {
+			System.out.println("No se ha podido insertar la Opinion 3");
+		}
+		
+		if (ClientManager.getInstance().insertarOpinion(opinion4)) {
+			System.out.println("Opinion 4 insertada correctamente.");
+		} else {
+			System.out.println("No se ha podido insertar la Opinion 4");
+		}
+		
+		if (ClientManager.getInstance().insertarOpinion(opinion5)) {
+			System.out.println("Opinion 5 insertada correctamente.");
+		} else {
+			System.out.println("No se ha podido insertar la Opinion 5");
+		}
+		
+		if (ClientManager.getInstance().insertarOpinion(opinionExistente)) {
+			System.out.println("Opinion Existente insertada correctamente.");
+		} else {
+			System.out.println("No se ha podido insertar la Opinion porque ya existe en la BD");
+		}
+		System.out.println();
+		
+		// Obtener Opiniones Usuario
+		System.out.println("Opiniones del Usuario 'Usuario 1':");
+		ArrayList<Opinion> opinionesObtenidas = ClientManager.getInstance().obtenerOpinionesUsuario("Usuario 1");
+		if (opinionesObtenidas.size() == 0) {
+			System.out.println("No existen opiniones para este usuario.");
+		} else {
+			for (Opinion o : opinionesObtenidas) {
+				System.out.println(o.toString());
+			}
+		}
+		System.out.println();
+		
+		System.out.println("Opiniones del Usuario 'Usuario 5':");
+		opinionesObtenidas = ClientManager.getInstance().obtenerOpinionesUsuario("Usuario 5");
+		if (opinionesObtenidas.size() == 0) {
+			System.out.println("No existen opiniones para este usuario.");
+		} else {
+			for (Opinion o : opinionesObtenidas) {
+				System.out.println(o.toString());
+			}
+		}
+		System.out.println();
+		
+		// Obtener Opiniones Excursion
+		System.out.println("Opiniones de la excursion 'Ruta del Cares':");
+		opinionesObtenidas = ClientManager.getInstance().obtenerOpinionesExcursion(1);
+		if (opinionesObtenidas.size() == 0) {
+			System.out.println("No existen opiniones sobre esta excursión");
+		} else {
+			for (Opinion o : opinionesObtenidas) {
+				System.out.println(o.toString());
+			}
+		}
+		System.out.println();
+		
+		System.out.println("Opiniones de la excursion 'Gorbea':");
+		opinionesObtenidas = ClientManager.getInstance().obtenerOpinionesExcursion(4);
+		if (opinionesObtenidas.size() == 0) {
+			System.out.println("No existen opiniones sobre esta excursión");
+		} else {
+			for (Opinion o : opinionesObtenidas) {
+				System.out.println(o.toString());
+			}
+		}
+		System.out.println();
+		
+		// Editar opinion
+		System.out.println("Editamos la opinion del Usuario 1 sobre la Ventana Relux:");
+		opinion3.setIdOpinion(3);
+		opinion3.setOpinion("Hemos editado esta opinión");
+		if (ClientManager.getInstance().editarOpinion(opinion3)) {
+			System.out.println("La Opinion 3 se ha editado correctamente.");
+		} else {
+			System.out.println("NO se ha podido editar la Opinion 3");
+		}
+		System.out.println();
+		
+		// Eliminar opinion
+		System.out.println("Eliminamos la opinion del Usuario 3 sobre la ruta del Cares:");
+		ClientManager.getInstance().eliminarOpinion(5);
+		System.out.println("La Opinion 5 se ha eliminado correctamente.");
 	}
 }
